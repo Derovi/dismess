c8f26b252e8ea88de865cc024cba94f9e50c9a53
derovi <dirbrains@gmail.com>
2021-04-04 06:03:31 +0300

    Added NetworkService

--- src/main/kotlin/by/dismess/core/network/NetworkMessage.kt
+++ src/main/kotlin/by/dismess/core/network/NetworkMessage.kt
@@ -1,6 +1,10 @@
 package by.dismess.core.network
 
+import java.net.InetAddress
+
 data class NetworkMessage(
     var tag: String,
     var data: String
-)
+) {
+    lateinit var senderAddress: InetAddress
+}
--- src/main/kotlin/by/dismess/core/services/NetworkService.kt
+++ src/main/kotlin/by/dismess/core/services/NetworkService.kt
@@ -1,17 +1,32 @@
 package by.dismess.core.services
 
+import by.dismess.core.klaxon
+import by.dismess.core.network.NetworkMessage
 import by.dismess.core.outer.NetworkInterface
+import java.net.InetSocketAddress
 
 class NetworkService(
-    networkInterface: NetworkInterface
+    val networkInterface: NetworkInterface
 ) {
     /**
      * Tag to list of registered handlers
      */
-    private val handlers = mutableMapOf<String, MutableList<(data: String) -> Unit>>()
+    private val handlers = mutableMapOf<String, MutableList<(message: NetworkMessage) -> Unit>>()
+        .withDefault { mutableListOf() }
     init {
         networkInterface.setMessageReceiver { sender, data ->
-
+            val message = klaxon.parse<NetworkMessage>(String(data)) ?: return@setMessageReceiver
+            message.senderAddress = sender
+            for (handler in handlers[message.tag]!!) { // handlers has default mutableList
+                handler(message)
+            }
         }
+    }
+    fun registerHandler(tag: String, handler: (message: NetworkMessage) -> Unit) {
+        handlers[tag]!!.add(handler) // has default mutableList
+    }
+    fun sendMessage(address: InetSocketAddress, tag: String, data: String) {
+        val message = NetworkMessage(tag, data)
+        networkInterface.sendRawMessage(address, klaxon.toJsonString(message).toByteArray())
     }
 }
