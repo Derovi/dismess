8131d0625948f5daadd332c0fda6b8e63618abd5
IgorFilipovich <60926970+IgorFilipovich@users.noreply.github.com>
2021-05-05 00:28:37 +0300

    Start SecureNetworkInterface (#12)
    * Start SecurityNetworkInterface
    
    Add ecnryption with use of AES256 symmetric ecnryption and DiffieHellman
    assymetric encryption.
    Implement Dismess Protocol.
    Add unit-tests for Encryptor and SecureNetworkInterface
    Finish SecureNetworkInterface with session management

--- src/main/kotlin/by/dismess/core/utils/utils.kt
+++ src/main/kotlin/by/dismess/core/utils/utils.kt
@@ -2,6 +2,7 @@ package by.dismess.core.utils
 
 import by.dismess.core.model.UserID
 import java.math.BigInteger
+import java.nio.ByteBuffer
 import java.security.MessageDigest
 
 fun hashMD5(input: String): BigInteger {
@@ -9,6 +10,10 @@ fun hashMD5(input: String): BigInteger {
     return BigInteger(1, md.digest(input.toByteArray(Charsets.UTF_8)))
 }
 
-fun generateUserID(login: String): UserID {
-    return UserID(hashMD5(login))
-}
+fun generateUserID(login: String): UserID = UserID(hashMD5(login))
+
+fun twoBytesToInt(number: ByteArray): Int = (number[0].toInt() and 0xff shl 8) or
+    (number[1].toInt() and 0xff)
+
+fun intToBytes(number: Int, size: Int = 1): ByteArray =
+    ByteBuffer.allocate(4).putInt(number).array().sliceArray((4 - size)..3)
