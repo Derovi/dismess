35dfa9f7cc8f385d1a318cf540e8d17507bc8057
Igor Filipovich <igor.filipovich02@gmail.com>
2021-05-06 21:27:20 +0300

    Implement RUDPNetworkInterface
    Implement NetworkInterface using reliable UDP
    RUDPNetworkInterfaceImpl starts udp server on passed address and send messages
    from the same address

---
+++ src/test/kotlin/by/dismess/core/network/RUDPNetworkInterfaceTest.kt
@@ -0,0 +1,166 @@
+package by.dismess.core.network
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import kotlin.random.Random
+
+class RUDPNetworkInterfaceTest {
+    private lateinit var message: ByteArray
+    private var sendCounter: Int = 0
+    private var receiveCounter: Int = 0
+
+    private fun init(): Pair<RUDPNetworkInterfaceImpl, RUDPNetworkInterfaceImpl> {
+        val first = RUDPNetworkInterfaceImpl()
+        val second = RUDPNetworkInterfaceImpl()
+        val receiver = { _: InetSocketAddress, data: ByteArray ->
+            Assert.assertArrayEquals(message, data)
+            ++receiveCounter
+            Unit
+        }
+        first.setMessageReceiver(receiver)
+        second.setMessageReceiver(receiver)
+        return Pair(first, second)
+    }
+
+    private suspend fun send(rudp: RUDPNetworkInterfaceImpl, port: Int, message: ByteArray) {
+        rudp.sendRawMessage(InetSocketAddress(port), message)
+        ++sendCounter
+    }
+
+    @Test
+    fun testSimple() {
+        val (first, second) = init()
+        message = "Дарова".toByteArray()
+        runBlocking {
+            first.start(InetSocketAddress(1234))
+            second.start(InetSocketAddress(2228))
+            send(first, 2228, message)
+            send(second, 1234, message)
+            Assert.assertEquals(sendCounter, receiveCounter)
+            Assert.assertNotEquals(receiveCounter, 0)
+            first.stop()
+            second.stop()
+        }
+    }
+
+    @Test
+    fun testBigData() {
+        val (first, second) = init()
+        message = ByteArray(200000) { 65 }
+        runBlocking {
+            first.start(InetSocketAddress(1234))
+            second.start(InetSocketAddress(2228))
+            send(first, 2228, message)
+            send(second, 1234, message)
+            Assert.assertEquals(sendCounter, receiveCounter)
+            Assert.assertNotEquals(receiveCounter, 0)
+            first.stop()
+            second.stop()
+        }
+    }
+
+    @Test
+    fun testSimpleStress() {
+        val (first, second) = init()
+        message = ByteArray(200) { 65 }
+        runBlocking {
+            first.start(InetSocketAddress(1234))
+            second.start(InetSocketAddress(2228))
+            repeat(1000) {
+                send(first, 2228, message)
+                send(second, 1234, message)
+            }
+            Assert.assertEquals(sendCounter, receiveCounter)
+            Assert.assertNotEquals(receiveCounter, 0)
+            first.stop()
+            second.stop()
+        }
+    }
+
+    @Test
+    fun testStressOrder() {
+        val first = RUDPNetworkInterfaceImpl()
+        val second = RUDPNetworkInterfaceImpl()
+        val receiver = { _: InetSocketAddress, data: ByteArray ->
+            Assert.assertArrayEquals(byteArrayOf(receiveCounter.toByte()), data)
+            receiveCounter += 1
+        }
+        first.setMessageReceiver(receiver)
+        second.setMessageReceiver(receiver)
+        runBlocking {
+            first.start(InetSocketAddress(1234))
+            second.start(InetSocketAddress(2228))
+            repeat(1000) {
+                message = byteArrayOf(sendCounter.toByte())
+                send(first, 2228, message)
+            }
+            Assert.assertEquals(sendCounter, receiveCounter)
+            Assert.assertNotEquals(receiveCounter, 0)
+            first.stop()
+            second.stop()
+        }
+    }
+
+    private fun findFreePort() = ServerSocket(0).use { it.localPort }
+
+    private fun generateFromTo(): Pair<Int, Int> {
+        val from = (0 until 10).random()
+        var to = (0 until 10).random()
+        while (to == from) {
+            to = (0 until 10).random()
+        }
+        return Pair(from, to)
+    }
+
+    @Test
+    fun testStressMultipleUsers() {
+        val usersNumber = 10
+        val sockets: MutableList<InetSocketAddress> = mutableListOf()
+        repeat(usersNumber) {
+            sockets.add(InetSocketAddress(findFreePort()))
+        }
+        val users: MutableList<RUDPNetworkInterfaceImpl> = mutableListOf()
+        val messages: MutableList<MutableList<MutableList<ByteArray>>> = mutableListOf()
+        for (i in 0 until usersNumber) {
+            messages.add(mutableListOf())
+            for (j in 0 until usersNumber) {
+                messages[i].add(mutableListOf())
+            }
+        }
+        runBlocking {
+            for (i in 0 until sockets.size) {
+                val user = RUDPNetworkInterfaceImpl()
+                user.start(sockets[i])
+                user.setMessageReceiver { sender: InetSocketAddress, data: ByteArray ->
+                    var from = 0
+                    for (j in 0 until sockets.size) {
+                        if (sockets[j].port == sender.port) {
+                            from = j
+                        }
+                    }
+                    val message = messages[from][i][0]
+                    messages[from][i].removeAt(0)
+                    Assert.assertArrayEquals(message, data)
+                    ++receiveCounter
+                    Unit
+                }
+                users.add(user)
+            }
+            repeat(1000) {
+                val (from, to) = generateFromTo()
+                message = ByteArray(200)
+                Random.nextBytes(message)
+                messages[from][to].add(message)
+                send(users[from], sockets[to].port, message)
+            }
+            Assert.assertEquals(receiveCounter, sendCounter)
+            Assert.assertNotEquals(receiveCounter, 0)
+            for (user in users) {
+                user.stop()
+            }
+        }
+    }
+}
