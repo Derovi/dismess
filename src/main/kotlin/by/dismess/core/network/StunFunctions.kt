a26fb1617de58460c40195ac517b3b14f11b6921
VKuzia <v.kuzia.mail@gmail.com>
2021-05-08 23:15:31 +0300

    Add JSTUN public public socket address getter

---
+++ src/main/kotlin/by/dismess/core/network/StunFunctions.kt
@@ -0,0 +1,31 @@
+package by.dismess.core.network
+
+import de.javawi.jstun.attribute.ChangeRequest
+import de.javawi.jstun.attribute.MappedAddress
+import de.javawi.jstun.attribute.MessageAttributeInterface
+import de.javawi.jstun.header.MessageHeader
+import de.javawi.jstun.header.MessageHeaderInterface
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.InetSocketAddress
+
+fun getPublicSocketAddress(): InetSocketAddress {
+    val sendMessageHeader = MessageHeader(MessageHeaderInterface.MessageHeaderType.BindingRequest)
+    val changeRequest = ChangeRequest()  // JSTUN requires empty request to be attached
+    sendMessageHeader.addMessageAttribute(changeRequest)
+    val data: ByteArray = sendMessageHeader.bytes
+
+    val socket = DatagramSocket()
+    socket.reuseAddress = true
+    val packet = DatagramPacket(data, data.size, InetAddress.getByName("stun.l.google.com"), 19302)
+    socket.send(packet)
+    val receivedPacket = DatagramPacket(ByteArray(32), 32)
+    socket.receive(receivedPacket)
+
+    val receiveMessageHeader = MessageHeader(MessageHeaderInterface.MessageHeaderType.BindingResponse)
+    receiveMessageHeader.parseAttributes(receivedPacket.data)
+    val mappedAddress: MappedAddress = receiveMessageHeader
+        .getMessageAttribute(MessageAttributeInterface.MessageAttributeType.MappedAddress) as MappedAddress
+    return InetSocketAddress(mappedAddress.address.inetAddress, mappedAddress.port)
+}
