cc9acfe7da54f01cc2c0752ed363103784a1d8a4
Igor Filipovich <igor.filipovich02@gmail.com>
2021-05-10 22:34:19 +0300

    Fix ktlint

--- src/main/kotlin/by/dismess/core/dht/DHTImpl.kt
+++ src/main/kotlin/by/dismess/core/dht/DHTImpl.kt
@@ -4,7 +4,6 @@ import by.dismess.core.model.UserID
 import by.dismess.core.services.NetworkService
 import by.dismess.core.services.StorageService
 import by.dismess.core.utils.UniqID
-import java.math.BigInteger
 import java.net.InetSocketAddress
 
 const val BUCKET_SIZE = 8
@@ -23,12 +22,12 @@ class DHTImpl(
     override suspend fun retrieve(key: String): ByteArray? {
         TODO("Not implemented yet")
     }
-  
+
     override suspend fun find(userId: UserID): InetSocketAddress? {
         TODO("Not yet implemented")
     }
 
-  
+
     override suspend fun remember(users: List<Map.Entry<UniqID, InetSocketAddress>>) {
         TODO("Not yet implemented")
     }
